54ca06f64293a0081dacb60b46459368a54d52d9
@daaronr <8229168+daaronr@users.noreply.github.com>
2022-03-17 15:18:18 -0400

    rebuild and add 'zip and copy' bit

--- main.R
+++ main.R
@@ -12,11 +12,13 @@ p_load(plotly)
 p_load(plm)
 p_load(pryr)
 p_load(stringr)
-p_load(kableExtra)
+p_load(stringr)
+p_load(effects)
 p_load(devtools)
 p_load(googledrive)
 p_load(googlesheets4)
 p_load(esquisse)
+p_load(rsconnect) #for rstudio 'private access' pubilshing
 devtools::install_github("paulhendricks/anonymizer")
 library(anonymizer)
 
@@ -26,7 +28,7 @@ library(anonymizer)
 
 devtools::install_github("rethinkpriorities/rp-r-package")
 library(rethinkpriorities)
-
+  
 devtools::install_github("rethinkpriorities/r-noodling-package")
 library(rnoodling)
 
@@ -39,6 +41,7 @@ knitr::write_bib(
   file = "assets/bib/packages.bib"
 )
 
+
 # For a list of 'rethink packages' and common packages we install, see: LINK HERE ####
 
 
@@ -46,6 +49,21 @@ knitr::write_bib(
   options(knitr.duplicate.label = "allow")
   rmarkdown::render_site(output_format = 'rethinkpriorities::book', encoding = 'UTF-8')
 }
+
+
+# Zip and move to dropbox (latter is not portable code) ####
+
+PASSWORD <- "reinstein"
+
+files2zip <- dir('docs', full.names = TRUE)
+
+zip(zipfile = 'eamt_bookdown',
+  files = files2zip,
+  flags = paste("--password", PASSWORD)
+  )
+
+file.copy(from = "eamt_bookdown.zip",
+  to   = "Dropbox/ea_marketing_misc_for_sharing/eamt_bookdown.zip")
 
 #bookdown::publish_book()
 
